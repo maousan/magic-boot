d7b7906d9724643eaa60e88f35734f895ea8565a
maou <362477892@qq.com>
2026-03-05 09:08:34 +0800

    first commit

---
+++ magic-api-plugins/magic-api-plugin-liteflow/src/main/java/org/ssssssss/magicapi/liteflow/LiteflowPlugin.java
@@ -0,0 +1,8 @@
+package org.ssssssss.magicapi.liteflow;
+
+/**
+ * LiteFlow plugin entry point (placeholder for skeleton).
+ */
+public class LiteflowPlugin {
+    // Placeholder for future auto-configuration
+}
